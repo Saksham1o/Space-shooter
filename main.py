829c5013a64edaae5a2d622327debdc5d428767c
Saksham1o <sakshampatiyar26@gmail.com>
2024-08-07 13:37:50 +0530

    creating window and main menu

---
+++ main.py
@@ -0,0 +1,95 @@
+from __future__ import division
+import pygame
+import random
+from os import path
+
+## img folder
+img_dir = path.join(path.dirname(__file__), 'img')
+sound_folder = path.join(path.dirname(__file__), 'sounds')
+
+###############################
+WIDTH = 480
+HEIGHT = 600
+FPS = 60
+POWERUP_TIME = 5000
+BAR_LENGTH = 100
+BAR_HEIGHT = 10
+
+# Define Colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+###############################
+
+
+## initialize pygame and create window
+pygame.init()
+pygame.mixer.init()  ## For sound
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Space Shooter")
+clock = pygame.time.Clock()     ## For syncing the FPS
+###############################
+
+font_name = pygame.font.match_font('arial')
+
+def main_menu():
+    global screen
+
+    menu_song = pygame.mixer.music.load(path.join(sound_folder, "C:\\Users\\Saksham\\Desktop\\git27\\Space-shooter\\sounds\\menu.org"))
+    pygame.mixer.music.play(-1)
+
+    title = pygame.image.load(path.join(img_dir, "C:\\Users\\Saksham\\Desktop\\git27\\Space-shooter\\img\\main.png")).convert()
+    title = pygame.transform.scale(title, (WIDTH, HEIGHT), screen)
+
+    screen.blit(title, (0,0))
+    pygame.display.update()
+
+    while True:
+        ev = pygame.event.poll()
+        if ev.type == pygame.KEYDOWN:
+            if ev.key == pygame.K_RETURN:
+                break
+            elif ev.key == pygame.K_q:
+                pygame.quit()
+                quit()
+        elif ev.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+        else:
+            draw_text(screen, "Press [ENTER] To Begin", 30, WIDTH/2, HEIGHT/2)
+            draw_text(screen, "or [Q] To Quit", 30, WIDTH/2, (HEIGHT/2)+40)
+            pygame.display.update()
+
+    #pygame.mixer.music.stop()
+    ready = pygame.mixer.Sound(path.join(sound_folder,"C:\\Users\\Saksham\\Desktop\\git27\\Space-shooter\\sounds\\getready.ogg"))
+    ready.play()
+    screen.fill(BLACK)
+    draw_text(screen, "GET READY!", 40, WIDTH/2, HEIGHT/2)
+    pygame.display.update()
+
+
+def draw_text(surf, text, size, x, y):
+    ## selecting a cross platform font to display the score
+    font = pygame.font.Font(font_name, size)
+    text_surface = font.render(text, True, WHITE)       ## True denotes the font to be anti-aliased
+    text_rect = text_surface.get_rect()
+    text_rect.midtop = (x, y)
+    surf.blit(text_surface, text_rect)
+
+
+def draw_shield_bar(surf, x, y, pct):
+    # if pct < 0:
+    #     pct = 0
+    pct = max(pct, 0)
+    ## moving them to top
+    # BAR_LENGTH = 100
+    # BAR_HEIGHT = 10
+    fill = (pct / 100) * BAR_LENGTH
+    outline_rect = pygame.Rect(x, y, BAR_LENGTH, BAR_HEIGHT)
+    fill_rect = pygame.Rect(x, y, fill, BAR_HEIGHT)
+    pygame.draw.rect(surf, GREEN, fill_rect)
+    pygame.draw.rect(surf, WHITE, outline_rect, 2)
+
